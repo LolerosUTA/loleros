e65d9e1bc65b660ff011a397bb3bc8dbaea23e55
ShinoN1 <floresvicente530@gmail.com>
2024-04-08 06:47:37 -0400

    Create calcchafa.py

---
+++ calcchafa.py
@@ -0,0 +1,43 @@
+
+def suma (a, b):
+    
+    return (a + b)
+
+    def resta(a, b):
+        
+        return (a - b)
+    
+    def multiplicacion (a, b):
+        
+        return (a * b)
+    
+    def division (a, b):
+        
+        if b == 0:
+            return " El resultado de la operacion no es valida"
+            
+        else:
+              return (a / b)
+
+print (" Bienvenido a la calculadora")
+print(" 1. suma")
+print(" 2. resta")
+print(" 3. multiplicacion")
+print(" 4. division")
+print(" 5. salir")
+
+opcion = int(input(" Seleccione la opcion 1/2/3/4/5 "))
+
+num1 = int(input( " Ingrese numero a"))
+num2 = int(input( " Ingrese numero b"))
+
+
+If opcion == "1":
+    print("El valor de la suma es: ", num1 + num2)
+
+        
+
+         
+    
+    
+    
